0c9d55f2531f74aef6f6b81265057fe341fba36d
Karimarouchi <abdelkarim.arouchi@esprit.tn>
2025-11-11 15:42:26 +0000

    Test Semgrep auto-fail with vulnerability

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("✅ Application DevSecOps Demo is running!");
+System.out.println("Password: " + request.getParameter("password"));
+
     }
 }
