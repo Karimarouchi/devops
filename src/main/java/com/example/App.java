64ec021f8b74c291b9a408c3a3f5ed520db3700f
Karimarouchi <abdelkarim.arouchi@esprit.tn>
2025-11-11 17:47:11 +0000

    Add vulnerable App.java and Semgrep DevSecOps GitHub Action

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,18 +7,17 @@ public class App {
     public static void main(String[] args) {
         System.out.println("✅ Application DevSecOps Demo is running!");
     }
-//java  code 
-    
+
+
     public void insecureLog(HttpServletRequest request) {
         // Semgrep doit détecter l'affichage du password
         System.out.println("Password: " + request.getParameter("password"));
     }
 
-    
+
    public void getUser(HttpServletRequest request, Connection conn) throws SQLException {
         String user = request.getParameter("username");
         Statement stmt = conn.createStatement();
-        
         ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + user + "'");
         while (rs.next()) {
             System.out.println(rs.getString(1));
