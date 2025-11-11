219b4a643d93fcfd70a1d08068d9caa348e9c805
Karimarouchi <abdelkarim.arouchi@esprit.tn>
2025-11-11 19:27:35 +0000

    Simplify App.java to remove servlet dependency

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,21 +1,31 @@
 package com.example;
 
-import java.sql.*;
-//import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// ✅ Classe factice pour remplacer HttpServletRequest
+class FakeRequest {
+    public String getParameter(String name) {
+        if (name.equals("username")) return "admin";
+        if (name.equals("password")) return "1234";
+        return "";
+    }
+}
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("✅ Applicationnnn DevSecOps Demo is running!");
+        System.out.println("✅ Application DevSecOps Demo is running!");
     }
 
-
-    public void insecureLog(HttpServletRequest request) {
-        // Semgrep doit détecter l'affichage du password
+    // 🔥 Vulnérabilité 1 : log d’un mot de passe
+    public void insecureLog(FakeRequest request) {
         System.out.println("Password: " + request.getParameter("password"));
     }
 
-
-   public void getUser(HttpServletRequest request, Connection conn) throws SQLException {
+    // 🔥 Vulnérabilité 2 : Injection SQL
+    public void getUser(FakeRequest request, Connection conn) throws SQLException {
         String user = request.getParameter("username");
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + user + "'");
