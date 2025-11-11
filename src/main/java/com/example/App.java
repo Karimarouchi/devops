ac8327c160b67f6ae61da36561e5a204e590ca6a
Karimarouchi <abdelkarim.arouchi@esprit.tn>
2025-11-11 17:17:51 +0000

    Test SQL injection detection with improved Semgrep rule

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,17 +8,17 @@ public class App {
         System.out.println("✅ Application DevSecOps Demo is running!");
     }
 
-    // 🔥 Vulnérabilité 1 : Log de mot de passe (insecure logging)
+    
     public void insecureLog(HttpServletRequest request) {
         // Semgrep doit détecter l'affichage du password
         System.out.println("Password: " + request.getParameter("password"));
     }
 
-    // 🔥 Vulnérabilité 2 : Injection SQL (concatenation non-sanitized)
-    public void getUser(HttpServletRequest request, Connection conn) throws SQLException {
+    
+   public void getUser(HttpServletRequest request, Connection conn) throws SQLException {
         String user = request.getParameter("username");
         Statement stmt = conn.createStatement();
-        // Requête vulnérable (SQL injection)
+        
         ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + user + "'");
         while (rs.next()) {
             System.out.println(rs.getString(1));
